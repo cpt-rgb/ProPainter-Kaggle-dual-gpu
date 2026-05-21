5fac2e2d061b06deb9eb125f92ca9b664e5c891c
cpt-rgb <149518311+cpt-rgb@users.noreply.github.com>
2026-05-21 20:24:34 +0530

    fdsfdsf

--- web-demos/hugging_face/inpainter/base_inpainter.py
+++ web-demos/hugging_face/inpainter/base_inpainter.py
@@ -170,8 +170,10 @@ class ProInpainter:
 		##############################################
 		# set up RAFT and flow competition model
 		##############################################
-		self.dev0 = self.device
-		self.dev1 = self.device
+		self.dev0 = 'cuda:0'
+		self.dev1 = 'cuda:1' if torch.cuda.device_count() > 1 else 'cuda:0'
+		# self.dev0 = self.device
+		# self.dev1 = self.device
 
 		self.fix_raft = RAFT_bi(raft_checkpoint, self.dev0)
 
